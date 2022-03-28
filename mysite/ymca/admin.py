a3abe08b176ba2d3857691646d1e03f81c5a9a31
DanilaF2020 <61852920+DanilaF2020@users.noreply.github.com>
2022-03-27 21:45:18 -0500

    Add files via upload

---
+++ mysite/ymca/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Event
+from .models import User
+# from .models import Role
+# Register your models here.
+admin.site.register(Event)
+admin.site.register(User)
+# admin.site.register(Role)
